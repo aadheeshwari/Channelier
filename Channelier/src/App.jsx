f22e3b53818483a02b47eaa19418f52706129d94
vinoth <vinoth@afreespace.com>
2022-10-17 10:40:11 +0530

    My Project

---
+++ Channelier/src/App.jsx
@@ -0,0 +1,12 @@
+import React from "react"
+import Home from "./Home"
+import Benefit from "./Benefit"
+
+export default function App() {
+  return (
+    <main>
+      <Home/>
+      <Benefit/>
+    </main>
+  )
+}
